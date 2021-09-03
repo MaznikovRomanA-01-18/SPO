30c23685f8706e5f9d7ca38eff260412a9a68c45
MaznikovRomanA-01-18 <90057146+MaznikovRomanA-01-18@users.noreply.github.com>
2021-09-03 19:02:42 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+
+#define INFO_BUFFER_SIZE 32767
+#define BUFSIZE MAX_PATH
+#define WINVER 0x0502
+#define MAX_DATA_LENGTH 16383
+using namespace std;
+
+int main()
+{
+    // 1.1 Версия операционной системы
+
+    OSVERSIONINFO osvi = {0};
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+    GetVersionEx(&osvi);
+
+    DWORD dwMinorVersion = osvi.dwMinorVersion;
+    DWORD dwMajorVersion = osvi.dwMajorVersion;
+    DWORD dwBuildNumber = osvi.dwBuildNumber;
+
+    printf("Version is %d.%d (%d)\n",
+                dwMajorVersion,
+                dwMinorVersion,
+                dwBuildNumber);
+    printf("--------------------------------\n");
+
+    // 1.2 Системный каталог
+
+    char system_dir[MAX_PATH];
+    GetSystemDirectory(system_dir, MAX_PATH);
+    printf("System directory: %s \n", system_dir);
+    printf("--------------------------------\n");
+
+    // 1.3 Название компьютера и псевдоним текущего пользователя
+
+    TCHAR  infoBuf[INFO_BUFFER_SIZE];
+    DWORD  bufCharCount = INFO_BUFFER_SIZE;
+
+    bufCharCount = INFO_BUFFER_SIZE;
+
+    GetComputerName(infoBuf, &bufCharCount);
+
+    bufCharCount = INFO_BUFFER_SIZE;
+    printf("Computer name: %s\n", infoBuf );
+
+    bufCharCount = INFO_BUFFER_SIZE;
+
+    GetUserName(infoBuf, &bufCharCount);
+
+    printf("User name: %s\n", infoBuf );
+    printf("--------------------------------\n");
+
+    // 1.4 Служебное имя тома, первый путь в файловой системе, объем тома и количество свободного места, доступного текущему
+    // пользователю
+
+    char buffer[MAX_PATH];
+    DWORD  cchBufferLength = MAX_PATH;
+    char Names[MAX_PATH];
+    __int64 total, available, free;
+
+
+    HANDLE search = FindFirstVolume(buffer, sizeof(buffer));
+
+    do {
+        printf("\n%s", buffer);
+        GetVolumePathNamesForVolumeName(buffer, Names, cchBufferLength, &cchBufferLength);
+        printf("\n  First path: %s", Names);
+        GetDiskFreeSpaceEx(
+                           buffer,
+                           (PULARGE_INTEGER)&available,
+                           (PULARGE_INTEGER)&total,
+                           (PULARGE_INTEGER)&free);
+
+        if (GetDiskFreeSpaceEx(
+                               buffer,
+                               (PULARGE_INTEGER)&available,
+                               (PULARGE_INTEGER)&total,
+                               (PULARGE_INTEGER)&free)==0)
+        {
+            printf("\n  No information available.\n ");
+        }
+        else
+        {
+        printf("\n  Total size: %u  bytes ", total);
+        printf("\n  Free space: %u  bytes\n", available);
+        }
+
+    } while (FindNextVolume(search, buffer, sizeof(buffer)));
+    FindVolumeClose(search);
+
+    printf("\n--------------------------------\n");
+
+    // 1.5  Cписок программ, запускаемых при старте системы, из реестра Windows
+
+    HKEY key;
+    DWORD dwIndex = 0;
+    TCHAR tcValue[MAX_PATH] = { 0 };
+    TCHAR lpValueName[MAX_DATA_LENGTH];
+    DWORD lpcchValueName = MAX_DATA_LENGTH;
+    DWORD lpDataLength = MAX_DATA_LENGTH;
+
+    long lResult = RegOpenKeyEx(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",0, KEY_READ, &key);
+    if (lResult == ERROR_SUCCESS)
+        {
+            while (RegEnumValue(key, dwIndex, lpValueName, &lpcchValueName, NULL, NULL, (LPBYTE)tcValue, &lpDataLength) == ERROR_SUCCESS)
+            {
+            printf("Programm %d: %s : %s \n", dwIndex+1, lpValueName, tcValue);
+            dwIndex++;
+            }
+        }
+
+    RegCloseKey(key);
+
+    printf("\n--------------------------------\n");
+
+    // 2. Добавить в программу функциональность измерения производительности ЦП
+
+    LARGE_INTEGER freq;
+    LARGE_INTEGER t_1;
+    LARGE_INTEGER t_2;
+
+    QueryPerformanceCounter(&t_1);
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&t_2);
+
+    double tact = t_2.QuadPart - t_1.QuadPart;
+    double frequency = freq.QuadPart;;
+    double us = 1000000 * tact / frequency;
+
+    printf("CPU Frequency = %u Hz\n",  freq);
+    printf("CPU clock = %f us\n",  us);
+}
